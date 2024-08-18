aec9bf4141e14f9040a773a9e2a8f9ff3150fd2b
thairyo <sonthai1310.works@gmail.com>
2024-08-18 09:08:08 +0700

    implementing MultiThreading

--- html_scraper/amazon_scraper_single.py
+++ html_scraper/amazon_scraper_single.py
@@ -2,15 +2,12 @@ from datetime import datetime
 import requests
 import csv
 import bs4
-import concurrent.futures
-from tqdm import tqdm
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
 REQUEST_HEADER = {
     'User-Agent': USER_AGENT,
     'Accept-Language': 'en-US, en;q=0.5',
 }
-NO_THREADS = 10
 
 def get_page_html(url):
     res = requests.get(url=url, headers=REQUEST_HEADER)
@@ -60,30 +57,31 @@ def get_product_details(soup):
             details[row_key] = row_value
         return details
 
-def extract_product_info(url, output):
+def extract_product_info(url):
     product_info = {}
-    # print(f'Scraping URL: {url}')
+    print(f'Scraping URL: {url}')
     html = get_page_html(url=url)
     soup = bs4.BeautifulSoup(html, 'lxml')
     product_info['price'] = get_product_price(soup)
     product_info['title'] = get_product_title(soup)
     product_info['rating'] = get_product_rating(soup)
     product_info.update(get_product_details(soup))
-    output.append(product_info)
+    return product_info
     
 
 if __name__ == '__main__':
     products_data = []
-    urls = []
     with open('amazon_products_urls.csv', newline='') as csvfile:
-        urls = list(csv.reader(csvfile, delimiter=','))
-    with concurrent.futures.ThreadPoolExecutor(max_workers=NO_THREADS) as executor:
-        for wkn in tqdm(range(0, len(urls))):
-            executor.submit(extract_product_info, urls[wkn][0], products_data)
-    output_file_name = 'output-{}.csv'.format(
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            url = row[0]
+            products_data.append(extract_product_info(url))
+    
+    output_file_name = 'output-{}-single.csv'.format(
         datetime.today().strftime("%d-%m-%Y"))
     with open(output_file_name, 'w') as outputfile:
         writer = csv.writer(outputfile)
         writer.writerow(products_data[0].keys())
         for product in products_data:
             writer.writerow(product.values())
+                
